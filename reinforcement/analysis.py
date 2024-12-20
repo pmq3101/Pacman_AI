a308cf15e9d2b4872332ce185c783e0dc1811e28
Quang <minhquangpham311@gmail.com>
2024-12-20 10:10:25 +0700

    Update Q2 Q3

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -19,59 +19,61 @@
 # Set the given parameters to obtain the specified policies through
 # value iteration.
 
-def question2a():
-    """
-      Prefer the close exit (+1), risking the cliff (-10).
-    """
-    answerDiscount = 0.1
-    answerNoise = 0.0
-    answerLivingReward = 0.0
-    return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
-
-def question2b():
-    """
-      Prefer the close exit (+1), but avoiding the cliff (-10).
-    """
-    answerDiscount = 0.1
-    answerNoise = 0.1
-    answerLivingReward = 0.0
-    return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
-
-def question2c():
-    """
-      Prefer the distant exit (+10), risking the cliff (-10).
-    """
+def question2():
     answerDiscount = 0.9
     answerNoise = 0.0
-    answerLivingReward = 0.0
+    return answerDiscount, answerNoise
+
+
+def question3a():
+    """ § Prefer the close exit (+1), risking the cliff (-10) """
+    answerDiscount = 0.1
+    answerNoise = 0.0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question2d():
-    """
-      Prefer the distant exit (+10), avoiding the cliff (-10).
-    """
+
+def question3b():
+    """ § Prefer the close exit (+1), but avoiding the cliff (-10) """
+    answerDiscount = 0.5
+    answerNoise = 0.3
+    answerLivingReward = -1
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+
+def question3c():
+    """ § Prefer the distant exit (+10), risking the cliff (-10) """
+    answerDiscount = 0.9
+    answerNoise = 0
+    answerLivingReward = 0
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+
+def question3d():
+    """ § Prefer the distant exit (+10), avoiding the cliff (-10) """
     answerDiscount = 0.9
     answerNoise = 0.3
-    answerLivingReward = 0.1
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
-def question2e():
-    """
-      Avoid both exits and the cliff (so an episode should never terminate).
-    """
-    answerDiscount = 0.01
-    answerNoise = 0.1
-    answerLivingReward = 100
+
+def question3e():
+    """ § Avoid both exits and the cliff (so an episode should never terminate) """
+    answerDiscount = 1
+    answerNoise = 0
+    answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 if __name__ == '__main__':
     print('Answers to analysis questions:')
     import analysis
+
     for q in [q for q in dir(analysis) if q.startswith('question')]:
         response = getattr(analysis, q)()
         print('  Question %s:\t%s' % (q, str(response)))
